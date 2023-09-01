d48dd32e26c9f20e08bcd8c170ffadf5d4acf126
pedrophalcadva <pedro_has12@hotmail.com>
2023-09-01 20:02:26 -0300

    Programacao_Back_End

---
+++ meu_script_em_php.php
@@ -0,0 +1,45 @@
+<?
+	$nome = 'Giovanni';
+	$idade = 39;
+?>
+
+<?
+	echo 'Meu nome é: ' .$nome
+?>
+
+<strong>
+	<?echo $idade  ?> Anos
+</strong>
+
+<?
+ $nome = (String) 'Giovanni';
+?>
+
+<?
+	$soma = 10 + 2;
+	$sub = 10 - 2;
+	$multi = 10 * 3;
+	$div = 10 / 3;
+?>
+
+<h1>
+	<? echo $soma ?>
+</h1>
+
+<h2>
+	<? echo $sub ?> 
+</h2>
+
+<?
+	$tom = 'Tom';
+	$firstTom = 'Hanks';
+	$secondTom = 'Cruise';
+?>
+
+<h1>
+	
+<?	echo $tom . ' ' . $firstTom. ' ' . $secondTom ?>
+
+</h1>
+
+
